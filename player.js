741469217d583748ff9d1556b54558b82fdd8f0d
apoorv1410 <43142014+apoorv1410@users.noreply.github.com>
2020-05-25 14:43:05 +0530

    Update player.js

--- player.js
+++ player.js
@@ -10,7 +10,7 @@ const tableheader =`
 
 
 const customHeaders = new Headers();
-customHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJleHAiOjE1OTAzOTQzMTUsImlhdCI6MTU5MDM3NjMxNX0.YNvtjw1js_WXb7DY6by08ZrCJKHg9Amn4x-TK1RZV890HnNTBPPj8_10bD_sq0A39bWkfZI66YaqFoCDaxIrIA");
+customHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJleHAiOjE1OTA0MTM3MTMsImlhdCI6MTU5MDM5NTcxM30.o9w7O-A16R1fk1jBWghzz6qOqwNlg-YXLRc-q2sTyp0Uj-m0z11T3VYen9ajErSH7gtTULFSku2frW1RQIBpYA");
 
     const requestOptions = {
     method: 'GET',
